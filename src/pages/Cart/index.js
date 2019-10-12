6876a3192a7f5354782f71e60e8e17749f3c794a
Leandro Rocha <prog.leandro@gmail.com>
2019-10-12 15:57:42 -0300

    Removendo produtos

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -8,7 +8,7 @@ import {
 
 import { Container, Total, ProductTable } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -22,7 +22,7 @@ function Cart({ cart }) {
         </thead>
         <tbody>
           {cart.map(product => (
-            <tr>
+            <tr key={product.id}>
               <td>
                 <img src={product.image} alt={product.title} />
               </td>
@@ -45,7 +45,12 @@ function Cart({ cart }) {
                 <strong>R$:258,80</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
